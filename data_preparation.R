3e1e69f4b4a5125377fda35e6e8b4568bd236e91
dwolffram <daniel-wolffram@gmx.de>
2021-09-09 13:45:47 +0200

    added preprocessed data for new ensembles

---
+++ data_preparation.R
@@ -0,0 +1,106 @@
+library(tidyverse)
+
+Sys.setlocale("LC_ALL", "C")
+
+VALID_TARGETS = c(paste(1:4, "wk ahead inc death"), paste(1:4, "wk ahead cum death"), paste(1:4, "wk ahead inc case"))
+
+MODELS_TO_EXCLUDE = c('COVIDhub-ensemble', 'COVIDhub-trained_ensemble', 
+                      'CU-nochange', 'CU-scenario_high', 'CU-scenario_low', 'CU-scenario_mid',
+                      'KITmetricslab-select_ensemble')
+
+LOCATIONS_TO_EXCLUDE = c("11", "60", "66", "69", "72", "74", "78")
+
+next_monday <- Vectorize(function(date){
+  date + (0:6)[weekdays(date + (0:6)) == "Monday"]
+  }
+)
+
+
+get_all_filepaths_and_dates <- function(){
+  files <- list.files(path=paste0("../covid19-forecast-hub/data-processed/"), 
+                      pattern=".csv$", full.names = TRUE, recursive=TRUE)
+  
+  df_files <- data.frame(filename = files)
+  df_files$model <- str_split(str_remove(df_files$filename, "../covid19-forecast-hub/data-processed//"),
+                              "/", simplify = TRUE)[, 1]
+  
+  df_files$forecast_date <- as.Date(str_sub(str_split(str_remove(df_files$filename, "../covid19-forecast-hub/data-processed//"), "/", simplify = TRUE)[, 2], end = 10))
+  
+  df_files$timezero <- sapply(df_files$forecast_date, next_monday)
+  df_files$timezero <- as.Date(df_files$timezero, origin = "1970-01-01")
+  df_files <- df_files %>%
+    filter(!(model %in% MODELS_TO_EXCLUDE) )
+}
+
+df_files <- get_all_filepaths_and_dates()
+
+load_file <- function(path, valid_targets, locations_to_exclude){
+  
+  df <- read_csv(path, col_types = cols(forecast_date = col_date(format = ""),
+                                        target = col_character(),
+                                        target_end_date = col_date(format = ""),
+                                        location = col_character(),
+                                        type = col_character(),
+                                        quantile = col_double(),
+                                        value = col_double()),
+                 progress = FALSE)
+  
+  df <- df %>%
+    filter(type == 'quantile',
+           target %in% valid_targets,
+           str_length(location) == 2,
+           !(location %in% locations_to_exclude)) %>%
+    drop_na()
+  
+  df <- df %>%
+    group_by(target, location) %>%
+    mutate(n_quantiles = n()) %>%
+    group_by(target) %>%
+    mutate(n_quantiles = min(n_quantiles)) %>%
+    filter(n_quantiles == 23 | (str_detect(target, 'inc case') & n_quantiles == 7)) %>%
+    select(-n_quantiles)
+  
+  df <- df %>%
+    group_by(target) %>%
+    filter(n_distinct(location) == 51)
+  
+  df <- df %>%
+    mutate(base_target = str_sub(target, 12)) %>%
+    group_by(location, base_target) %>%
+    mutate(n_horizons = n_distinct(target)) %>%
+    group_by(base_target) %>%
+    filter(min(n_horizons) == 4) %>%
+    ungroup() %>%
+    select(-c(n_horizons, base_target))
+}
+
+# path <- "../covid19-forecast-hub/data-processed//BPagano-RtDriven/2020-10-25-BPagano-RtDriven.csv"
+# 
+# d <- load_file(path, VALID_TARGETS, LOCATIONS_TO_EXCLUDE)
+
+
+
+pb <- txtProgressBar(0, nrow(df_files), style = 3)
+
+df <- data.frame()
+for (row in 1:nrow(df_files)){
+  setTxtProgressBar(pb, row)
+  df_temp <- load_file(df_files[row, "filename"], VALID_TARGETS, LOCATIONS_TO_EXCLUDE)
+  df_temp$model <- df_files[row, "model"]
+  
+  if (nrow(df_temp) == 0) {
+    next
+  } 
+  
+  df <- bind_rows(df, df_temp)
+}
+close(pb)
+
+write_csv(df, "data/df.csv.gz", row.names=FALSE)
+
+df_cd <- df %>%
+  filter(target %in% paste(1:4, "wk ahead cum death"))
+
+write_csv(df_cd, "data/df_cumulative_deaths.csv.gz")
+
+
---
+++ load_data.R
@@ -0,0 +1,112 @@
+library(tidyverse)
+
+Sys.setlocale("LC_ALL", "C")
+
+next_monday <- Vectorize(function(date){
+  date + (0:6)[weekdays(date + (0:6)) == "Monday"]
+  }
+)
+
+load_truth <- function(target="incident_cases", as_of){
+  truth <- read.csv(paste0("https://raw.githubusercontent.com/dwolffram/covid19-versioned-data/main/data/", 
+                           target, "/jhu_", target, "_as_of_", as_of, ".csv"), 
+                    colClasses = c(location="character", date ="Date"))
+}
+
+add_truth <- function(df, as_of){
+  target_dict = list("inc case" = "incident_cases",
+                     "inc death" = "incident_deaths",
+                     "cum death" = "cumulative_deaths")
+  
+  df$merge_target <- str_sub(df$target, start=12)
+  
+  targets <- unique(df$merge_target)
+  
+  truth_df <- data.frame()
+  for (target in targets){
+    truth <- load_truth(target_dict[[target]], as_of) %>%
+      rename(truth = value) %>%
+      mutate(merge_target = target)
+    
+    truth_df <- bind_rows(truth_df, truth)
+  }
+  
+  df <- df %>%
+    left_join(truth_df, by=c("merge_target", "target_end_date"="date", "location")) %>% 
+    select(- merge_target)
+  
+  return(df)
+}
+
+# load data for models that require complete submissions (window size 4)
+load_train_test <- function(forecast_date, national_level = FALSE){
+  df_train <- read_csv(paste0("data/", forecast_date, "_train.csv"), 
+                       col_types = cols(forecast_date = col_date(format = ""),
+                                        target = col_character(),
+                                        target_end_date = col_date(format = ""),
+                                        location = col_character(),
+                                        type = col_character(),
+                                        quantile = col_double(),
+                                        value = col_double())) %>%    
+    filter(if (!national_level) location != "US" else TRUE) %>%
+    as.data.frame()
+  
+  
+  df_test <- read_csv(paste0("data/", forecast_date, "_test.csv"), 
+                      col_types = cols(forecast_date = col_date(format = ""),
+                                       target = col_character(),
+                                       target_end_date = col_date(format = ""),
+                                       location = col_character(),
+                                       type = col_character(),
+                                       quantile = col_double(),
+                                       value = col_double())) %>%
+    filter(if (!national_level) {location != "US"} else {location == "US"}) %>%
+    as.data.frame()
+  
+  return(list(df_train=df_train, df_test=df_test))
+}
+
+
+load_data <- function(){
+  df <- read_csv("data/df_cumulative_deaths.csv.gz", col_types = cols(forecast_date = col_date(format = ""),
+                                                                 target = col_character(),
+                                                                 target_end_date = col_date(format = ""),
+                                                                 location = col_character(),
+                                                                 type = col_character(),
+                                                                 quantile = col_double(),
+                                                                 value = col_double()))
+  
+  # add timezero: the next Monday after forecast_date (in case the submission was made before Monday)
+  df <- df %>%
+    nest_by(forecast_date) %>%
+    mutate(timezero = as.Date(next_monday(forecast_date), origin = "1970-01-01")) %>%
+    unnest(cols = c(data))
+  
+  return(df)
+}
+
+
+# select train and test data for models that use the full history (with missing submissions)
+train_test_split <- function(df, test_date){
+  df_test <- df %>%
+    filter(timezero == test_date) %>%    
+    as.data.frame()
+  
+  
+  df_train <- df %>%
+    filter(target_end_date < test_date,
+           model %in% unique(df_test$model)) %>%
+    as.data.frame()
+  
+  df_train <- add_truth(df_train, as_of=test_date)
+  df_test$truth <- NA
+  
+  return(list(df_train=df_train, df_test=df_test))
+}
+
+
+# df <- load_data()
+# 
+# dfs <- train_test_split(df, "2021-07-19")
+# train <- dfs$df_train
+# test <- dfs$df_test
